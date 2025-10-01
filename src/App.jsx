bc2c5a6bb243d686d847a77d53c6c4b6158fb31b
Abhishek Choudhary <abhishekchoudharyorg@gmail.com>
2025-10-01 18:02:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react' 
-import './App.css'
+import './index.css'
 import Navbar from './components/Navbar'
 import Manager from './components/Manager'
 import Footer from './components/Footer'
